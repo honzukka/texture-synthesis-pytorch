ed08b04b8526f24c38cc503ea739d99d8622b6db
honzukka <randomnonplayercharacter@gmail.com>
2020-07-01 14:28:00 +0300

    Gatys model converted to PyTorch

---
+++ convert_model.py
@@ -0,0 +1,105 @@
+import argparse
+from collections import OrderedDict
+import os
+from types import SimpleNamespace
+
+import torch
+import torchvision
+
+import utilities
+import caffemodel2pytorch
+
+
+# the original Caffe model can be found at
+# http://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel
+def main(args=None):
+    return_value = True
+    if args is None:
+        args = parse_arguments()
+        return_value = False
+
+    caffe_model_path = os.path.join(args.model_dir, args.caffe_model)
+    pytorch_model_out_path = os.path.join(args.model_dir, args.out_name)
+    tmp_path = os.path.join(args.model_dir, 'temp.pt')
+
+    print('Converting normalized Caffe weights to a PyTorch state_dict...')
+    caffe_args = SimpleNamespace(
+        model_caffemodel=caffe_model_path,
+        output_path=tmp_path,
+        caffe_proto=''
+    )
+    caffemodel2pytorch.main(caffe_args)
+    state_dict = torch.load(tmp_path)
+
+    # reshape caffe bias to match the PyTorch one
+    for learnables_key in state_dict:
+        if 'bias' in learnables_key:
+            state_dict[learnables_key] = state_dict[learnables_key].squeeze()
+
+    print('Loading VGG19 PyTorch model...')
+
+    # only features are needed
+    net = torchvision.models.vgg19(pretrained=True).features
+
+    # rename VGG19's feature layers, so that we can refer to them easily later
+    new_layer_names = [
+        'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
+        'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
+        'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3',
+        'conv3_4', 'relu3_4', 'pool3',
+        'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3',
+        'conv4_4', 'relu4_4', 'pool4',
+        'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3',
+        'conv5_4', 'relu5_4', 'pool5'
+    ]
+    net = torch.nn.Sequential(
+        OrderedDict(zip(new_layer_names, net))
+    )
+
+    # replace max pooling by average pooling
+    for i in range(len(net)):
+        if isinstance(net[i], torch.nn.MaxPool2d):
+            net[i] = torch.nn.AvgPool2d(2, stride=2)
+
+    print('Loading normalized weights into the PyTorch model...')
+    net.load_state_dict(state_dict)
+
+    # remove intermediate files
+    os.remove(tmp_path)
+
+    if return_value:
+        return net
+    else:
+        print('Saving the converted model..')
+        utilities.save_model(net, pytorch_model_out_path)
+        return None
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        '--model_dir',
+        default='./models',
+        help='Directory containing models.'
+    )
+
+    parser.add_argument(
+        '--caffe_model',
+        default='vgg_normalised.caffemodel',
+        help='Caffe model weights file name.'
+    )
+
+    parser.add_argument(
+        '--out_name',
+        default='VGG19_normalized_avg_pool_pytorch',
+        help='PyTorch model weights output file name.'
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utilities.py
@@ -0,0 +1,76 @@
+import pickle
+import sys
+import imp
+import inspect
+import importlib
+
+
+def save_model(model, path):
+    """
+    Saves the model(s), including the definitions in its containing module.
+    Restore the model(s) with load_model. References to other modules
+    are not chased; they're assumed to be available when calling load_model.
+    The state of any other object in the module is not stored.
+    """
+    model_pickle = pickle.dumps(model)
+
+    # Handle dicts, lists and tuples of models.
+    model = list(model.values()) if isinstance(model, dict) else model
+    model = (
+        (model,)
+        if not (isinstance(model, list) or isinstance(model, tuple))
+        else model
+    )
+
+    # Create a dict of modules that maps from name to source code.
+    module_names = {m.__class__.__module__ for m in model}
+    modules = {
+        name:
+            inspect.getsource(importlib.import_module(name))
+            for name in module_names
+    }
+
+    pickle.dump((modules, model_pickle), open(path, 'wb'))
+
+
+def load_model(path):
+    """Loads the model(s) stored by save_model."""
+    modules, model_pickle = pickle.load(open(path, 'rb'))
+
+    # Temporarily add or replace available modules with stored ones.
+    sys_modules = {}
+    for name, source in modules.items():
+        module = imp.new_module(name)
+        exec(source, module.__dict__)
+        if name in sys.modules:
+            sys_modules[name] = sys.modules[name]
+        sys.modules[name] = module
+
+    # Map pytorch models to cpu if cuda is not available.
+    if imp.find_module('torch'):
+        import torch
+        original_load = torch.load
+
+        def map_location_cpu(*args, **kwargs):
+            kwargs['map_location'] = 'cpu'
+            return original_load(*args, **kwargs)
+        torch.load = (
+            original_load
+            if torch.cuda.is_available()
+            else map_location_cpu
+        )
+
+    model = pickle.loads(model_pickle)
+
+    if imp.find_module('torch'):
+        torch.load = original_load  # Revert monkey patch.
+
+    # Revert sys.modules to original state.
+    for name in modules.keys():
+        if name in sys_modules:
+            sys.modules[name] = sys_modules[name]
+        else:
+            # Just to make sure nobody else depends on these existing.
+            sys.modules.pop(name)
+
+    return model
